e0fc6cf2d8b8e633bb156a2051b314d303fc9a8c
Harold <smithhe95@protonmail.com>
2023-08-30 22:09:14 -0400

    Added remaining projects

---
+++ Api/Ecommerce.FastEndpoints/FastEndpontServiceRegistration.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.FastEndpoints
+{
+	public static class FastEndpontServiceRegistration
+	{
+		public static void AddFastEndpointServices(this IServiceCollection services)
+		{
+			
+		}
+	}
+}
