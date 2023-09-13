947fca54b582f372f02d4e4f4bb8950a4b771cf4
Harold <smithhe95@protonmail.com>
2023-09-13 17:06:35 -0400

    Added documentation to the request and Endpoint for updating a Review

--- Api/Ecommerce.FastEndpoints/Review/UpdateReviewEndpoint.cs
+++ Api/Ecommerce.FastEndpoints/Review/UpdateReviewEndpoint.cs
@@ -12,25 +12,42 @@ using System.Threading.Tasks;
 
 namespace Ecommerce.FastEndpoints.Review
 {
+	/// <summary>
+	/// A Fast Endpoint implementation that handles updating an existing Review
+	/// </summary>
 	public class UpdateReviewEndpoint : Endpoint<UpdateReviewApiRequest, UpdateReviewResponse>
 	{
 		private readonly ILogger<UpdateReviewEndpoint> _logger;
 		private readonly IMediator _mediator;
 		private readonly IAuthenticationService _authenticationService;
 
-		public UpdateReviewEndpoint(ILogger<UpdateReviewEndpoint> logger, IMediator mediator,
-			IAuthenticationService authenticationService)
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UpdateReviewEndpoint"/> class.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> instance used for logging.</param>
+		/// <param name="mediator">The <see cref="IMediator"/> instance used for sending Mediator requests.</param>
+		/// <param name="authenticationService">The <see cref="IAuthenticationService"/> instance used for token validation</param>
+		public UpdateReviewEndpoint(ILogger<UpdateReviewEndpoint> logger, IMediator mediator, IAuthenticationService authenticationService)
 		{
 			this._logger = logger;
 			this._mediator = mediator;
 			this._authenticationService = authenticationService;
 		}
 		
+		/// <summary>
+		/// Configures the route and roles for the Endpoint
+		/// </summary>
 		public override void Configure()
 		{
 			Post("/api/review/update");
+			//TODO: Add roles
 		}
 
+		/// <summary>
+		/// Handles the <see cref="UpdateReviewApiRequest"/> and generates a <see cref="UpdateReviewResponse"/> 
+		/// </summary>
+		/// <param name="req">The <see cref="UpdateReviewApiRequest"/> object sent in the HTTP request</param>
+		/// <param name="ct">The <see cref="CancellationToken"/> that can be used to request cancellation of the operation.</param>
 		public override async Task HandleAsync(UpdateReviewApiRequest req, CancellationToken ct)
 		{
 			this._logger.LogInformation("Handling Update Review Request");
@@ -45,9 +62,9 @@ namespace Ecommerce.FastEndpoints.Review
 			}
 
 			UpdateReviewResponse response;
-
 			try
 			{
+				//Send the update command
 				response = await this._mediator.Send(new UpdateReviewCommand
 				{
 					ReviewToUpdate = req.ReviewToUpdate,
@@ -56,12 +73,14 @@ namespace Ecommerce.FastEndpoints.Review
 			}
 			catch (Exception e)
 			{
+				//Unexpected error
 				this._logger.LogError(e, "Error when attempt to update review");
 				await SendAsync(new UpdateReviewResponse { Success = false, Message = "Unexpected Error Occurred" },
 					500, ct);
 				return;
 			}
 
+			//Send the response object
 			await SendOkAsync(response, ct);
 		}
 	}
--- Core/Ecommerce.Shared/Requests/Review/UpdateReviewApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Review/UpdateReviewApiRequest.cs
@@ -2,8 +2,14 @@ using Ecommerce.Shared.Dtos;
 
 namespace Ecommerce.Shared.Requests.Review
 {
+	/// <summary>
+	/// A Api request to update a Review
+	/// </summary>
 	public class UpdateReviewApiRequest
 	{
+		/// <summary>
+		/// The Review to update with
+		/// </summary>
 		public ReviewDto? ReviewToUpdate { get; set; }
 	}
 }
