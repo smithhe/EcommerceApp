a42809adcefffa320bc8b2170811b42bdad8753a
Harold <smithhe95@protonmail.com>
2023-09-13 16:14:56 -0400

    Updated request to get a Order by Id and added a Endpoint to handle the request

--- Core/Ecommerce.Shared/Requests/Order/GetOrderByIdApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Order/GetOrderByIdApiRequest.cs
@@ -1,7 +1,13 @@
 namespace Ecommerce.Shared.Requests.Order
 {
+	/// <summary>
+	/// A Api request to get a Order via its Id
+	/// </summary>
 	public class GetOrderByIdApiRequest
 	{
-		
+		/// <summary>
+		/// The unique identifier of the Order to retrieve
+		/// </summary>
+		public int Id { get; set; }
 	}
 }
