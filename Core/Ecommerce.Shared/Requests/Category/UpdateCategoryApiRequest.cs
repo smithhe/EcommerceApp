f33a0f6c3721fabce457980a97c879cc1775efc2
Harold <smithhe95@protonmail.com>
2023-09-13 13:52:18 -0400

    Updated request to update a Category and added a Endpoint to handle the request

--- Core/Ecommerce.Shared/Requests/Category/UpdateCategoryApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Category/UpdateCategoryApiRequest.cs
@@ -1,7 +1,15 @@
+using Ecommerce.Shared.Dtos;
+
 namespace Ecommerce.Shared.Requests.Category
 {
+	/// <summary>
+	/// A Api request to update a Category
+	/// </summary>
 	public class UpdateCategoryApiRequest
 	{
-		
+		/// <summary>
+		/// The Category to update with
+		/// </summary>
+		public CategoryDto? CategoryToUpdate { get; set; }
 	}
 }
