a256ce3c461ce3a53c7bd224107fb75e3cb86224
Harold <smithhe95@protonmail.com>
2023-09-13 17:03:42 -0400

    Added documentation to the request and Endpoint for deleting a Review

--- Api/Ecommerce.FastEndpoints/Review/DeleteReviewEndpoint.cs
+++ Api/Ecommerce.FastEndpoints/Review/DeleteReviewEndpoint.cs
@@ -12,25 +12,42 @@ using System.Threading.Tasks;
 
 namespace Ecommerce.FastEndpoints.Review
 {
+	/// <summary>
+	/// A Fast Endpoint implementation that handles deleting a Review
+	/// </summary>
 	public class DeleteReviewEndpoint : Endpoint<DeleteReviewApiRequest, DeleteReviewResponse>
 	{
 		private readonly ILogger<DeleteReviewEndpoint> _logger;
 		private readonly IMediator _mediator;
 		private readonly IAuthenticationService _authenticationService;
 
-		public DeleteReviewEndpoint(ILogger<DeleteReviewEndpoint> logger, IMediator mediator,
-			IAuthenticationService authenticationService)
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DeleteReviewEndpoint"/> class.
+		/// </summary>
+		/// <param name="logger">The <see cref="ILogger"/> instance used for logging.</param>
+		/// <param name="mediator">The <see cref="IMediator"/> instance used for sending Mediator requests.</param>
+		/// <param name="authenticationService">The <see cref="IAuthenticationService"/> instance used for token validation</param>
+		public DeleteReviewEndpoint(ILogger<DeleteReviewEndpoint> logger, IMediator mediator, IAuthenticationService authenticationService)
 		{
 			this._logger = logger;
 			this._mediator = mediator;
 			this._authenticationService = authenticationService;
 		}
 		
+		/// <summary>
+		/// Configures the route and roles for the Endpoint
+		/// </summary>
 		public override void Configure()
 		{
 			Post("/api/review/delete");
+			//TODO: Add roles
 		}
 
+		/// <summary>
+		/// Handles the <see cref="DeleteReviewApiRequest"/> and generates a <see cref="DeleteReviewResponse"/> 
+		/// </summary>
+		/// <param name="req">The <see cref="DeleteReviewApiRequest"/> object sent in the HTTP request</param>
+		/// <param name="ct">The <see cref="CancellationToken"/> that can be used to request cancellation of the operation.</param>
 		public override async Task HandleAsync(DeleteReviewApiRequest req, CancellationToken ct)
 		{
 			this._logger.LogInformation("Handling Delete Review Request");
@@ -45,19 +62,21 @@ namespace Ecommerce.FastEndpoints.Review
 			}
 
 			DeleteReviewResponse response;
-
 			try
 			{
+				//Send the delete command
 				response = await this._mediator.Send(new DeleteReviewCommand { ReviewToDelete = req.ReviewToDelete }, ct);
 			}
 			catch (Exception e)
 			{
+				//Unexpected error
 				this._logger.LogError(e, "Error when attempt to delete review");
 				await SendAsync(new DeleteReviewResponse { Success = false, Message = "Unexpected Error Occurred" },
 					500, ct);
 				return;
 			}
 
+			//Send the response object
 			await SendOkAsync(response, ct);
 		}
 	}
--- Core/Ecommerce.Shared/Requests/Review/DeleteReviewApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Review/DeleteReviewApiRequest.cs
@@ -2,8 +2,14 @@ using Ecommerce.Shared.Dtos;
 
 namespace Ecommerce.Shared.Requests.Review
 {
+	/// <summary>
+	/// A Api request to delete a Review
+	/// </summary>
 	public class DeleteReviewApiRequest
 	{
+		/// <summary>
+		/// The Review to delete
+		/// </summary>
 		public ReviewDto? ReviewToDelete { get; set; }
 	}
 }
