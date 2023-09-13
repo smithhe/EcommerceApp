3ab4dc946546c10c9ef3eea44caa440b1088f5d0
Harold <smithhe95@protonmail.com>
2023-09-13 17:13:08 -0400

    Added documentation to the request and Endpoint for getting a User's Review of a Product

--- Core/Ecommerce.Shared/Requests/Review/GetUserReviewForProductApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Review/GetUserReviewForProductApiRequest.cs
@@ -1,9 +1,18 @@
 namespace Ecommerce.Shared.Requests.Review
 {
+	/// <summary>
+	/// A Api request to get a User's review of a Product
+	/// </summary>
 	public class GetUserReviewForProductApiRequest
 	{
+		/// <summary>
+		/// The UserName of the User who made the Review
+		/// </summary>
 		public string? UserName { get; set; }
 		
+		/// <summary>
+		/// The unique identifier of the Product the Review is for
+		/// </summary>
 		public int ProductId { get; set; }
 	}
 }
