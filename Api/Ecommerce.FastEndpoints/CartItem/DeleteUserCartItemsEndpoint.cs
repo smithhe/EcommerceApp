eaef18b2c675727a160ab16542901385dac44b4a
Harold <smithhe95@protonmail.com>
2023-09-13 12:55:29 -0400

    Updated request to delete a User's CartItems and added Endpoint to handle the request

--- Api/Ecommerce.FastEndpoints/CartItem/DeleteUserCartItemsEndpoint.cs
+++ Api/Ecommerce.FastEndpoints/CartItem/DeleteUserCartItemsEndpoint.cs
@@ -1,4 +1,4 @@
-using Ecommerce.Application.Features.CartItem.Commands.DeleteCartItem;
+using Ecommerce.Application.Features.CartItem.Commands.DeleteUserCartItems;
 using Ecommerce.Identity.Contracts;
 using Ecommerce.Shared.Requests.CartItem;
 using Ecommerce.Shared.Responses.CartItem;
@@ -13,21 +13,21 @@ using System.Threading.Tasks;
 namespace Ecommerce.FastEndpoints.CartItem
 {
 	/// <summary>
-	/// A Fast Endpoint implementation that handles deleting a CartItem
+	/// A Fast Endpoint implementation that handles deleting a User's CartItems
 	/// </summary>
-	public class DeleteCartItemEndpoint : Endpoint<DeleteCartItemApiRequest, DeleteCartItemResponse>
+	public class DeleteUserCartItemsEndpoint : Endpoint<DeleteUserCartItemsApiRequest, DeleteUserCartItemsResponse>
 	{
-		private readonly ILogger<DeleteCartItemEndpoint> _logger;
+		private readonly ILogger<DeleteUserCartItemsEndpoint> _logger;
 		private readonly IMediator _mediator;
 		private readonly IAuthenticationService _authenticationService;
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="DeleteCartItemEndpoint"/> class.
+		/// Initializes a new instance of the <see cref="DeleteUserCartItemsEndpoint"/> class.
 		/// </summary>
 		/// <param name="logger">The <see cref="ILogger"/> instance used for logging.</param>
 		/// <param name="mediator">The <see cref="IMediator"/> instance used for sending Mediator requests.</param>
 		/// <param name="authenticationService">The <see cref="IAuthenticationService"/> instance used for token validation</param>
-		public DeleteCartItemEndpoint(ILogger<DeleteCartItemEndpoint> logger, IMediator mediator, IAuthenticationService authenticationService)
+		public DeleteUserCartItemsEndpoint(ILogger<DeleteUserCartItemsEndpoint> logger, IMediator mediator, IAuthenticationService authenticationService)
 		{
 			this._logger = logger;
 			this._mediator = mediator;
@@ -39,18 +39,18 @@ namespace Ecommerce.FastEndpoints.CartItem
 		/// </summary>
 		public override void Configure()
 		{
-			Post("/api/cartitem/delete");
+			Post("/api/cartitem/user/delete");
 			//TODO: Add roles
 		}
 
 		/// <summary>
-		/// Handles the <see cref="DeleteCartItemApiRequest"/> and generates a <see cref="DeleteCartItemResponse"/> 
+		/// Handles the <see cref="DeleteUserCartItemsApiRequest"/> and generates a <see cref="DeleteUserCartItemsResponse"/> 
 		/// </summary>
-		/// <param name="req">The <see cref="DeleteCartItemApiRequest"/> object sent in the HTTP request</param>
+		/// <param name="req">The <see cref="DeleteUserCartItemsApiRequest"/> object sent in the HTTP request</param>
 		/// <param name="ct">The <see cref="CancellationToken"/> that can be used to request cancellation of the operation.</param>
-		public override async Task HandleAsync(DeleteCartItemApiRequest req, CancellationToken ct)
+		public override async Task HandleAsync(DeleteUserCartItemsApiRequest req, CancellationToken ct)
 		{
-			this._logger.LogInformation("Handling Delete CartItem Request");
+			this._logger.LogInformation("Handling Delete User CartItems Request");
 			
 			//Check if token is valid
 			string? token = this.HttpContext.Request.Headers["Authorization"].FirstOrDefault();
@@ -61,27 +61,24 @@ namespace Ecommerce.FastEndpoints.CartItem
 				return;
 			}
 
-			DeleteCartItemResponse response;
+			DeleteUserCartItemsResponse response;
+			
 			try
 			{
 				//Send the delete command
-				response = await this._mediator.Send(new DeleteCartItemCommand
-				{
-					CartItemToDelete = req.CartItemToDelete, 
-				}, ct);
+				response = await this._mediator.Send(new DeleteUserCartItemsCommand { UserId = req.UserId }, ct);
 			}
 			catch (Exception e)
 			{
 				//Unexpected error
-				this._logger.LogError(e, "Error when attempt to delete CartItem");
-				await SendAsync(new DeleteCartItemResponse { Success = false, Message = "Unexpected Error Occurred" },
+				this._logger.LogError(e, "Error when attempt to delete user CartItems");
+				await SendAsync(new DeleteUserCartItemsResponse { Success = false, Message = "Unexpected Error Occurred" },
 					500, ct);
 				return;
 			}
 
 			//Send the response object
 			await SendOkAsync(response, ct);
-			
 		}
 	}
 }
--- Core/Ecommerce.Shared/Requests/CartItem/DeleteUserCartItemsApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/CartItem/DeleteUserCartItemsApiRequest.cs
@@ -2,8 +2,14 @@ using System;
 
 namespace Ecommerce.Shared.Requests.CartItem
 {
+	/// <summary>
+	/// A Api request to delete all CartItems for a User
+	/// </summary>
 	public class DeleteUserCartItemsApiRequest
 	{
+		/// <summary>
+		/// The unique identifier of the User to delete all CartItems for
+		/// </summary>
 		public Guid UserId { get; set; }
 	}
 }
