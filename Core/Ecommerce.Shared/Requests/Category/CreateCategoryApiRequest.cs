91b5ccc542a00e6a82b9ebdb95169970e63677a8
Harold <smithhe95@protonmail.com>
2023-09-13 13:33:27 -0400

    Updated request to create a Category and added a Endpoint to handle the request

--- Core/Ecommerce.Shared/Requests/Category/CreateCategoryApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Category/CreateCategoryApiRequest.cs
@@ -1,7 +1,15 @@
+using Ecommerce.Shared.Dtos;
+
 namespace Ecommerce.Shared.Requests.Category
 {
+	/// <summary>
+	/// A Api request to create a new Category
+	/// </summary>
 	public class CreateCategoryApiRequest
 	{
-		
+		/// <summary>
+		/// The Category to be created
+		/// </summary>
+		public CategoryDto? CategoryToCreate { get; set; }
 	}
 }
