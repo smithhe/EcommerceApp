babf37fe7302eedd0532d645384bdab2e440afb7
Harold <smithhe95@protonmail.com>
2023-09-13 16:00:53 -0400

    Updated request to get all Orders for a User and added a Endpoint to handle the request

--- Core/Ecommerce.Shared/Requests/Order/GetAllOrdersByUserIdApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Order/GetAllOrdersByUserIdApiRequest.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace Ecommerce.Shared.Requests.Order
 {
+	/// <summary>
+	/// A Api request to get all Orders for a User
+	/// </summary>
 	public class GetAllOrdersByUserIdApiRequest
 	{
-		
+		/// <summary>
+		/// Id of the User to find all Orders for
+		/// </summary>
+		public Guid UserId { get; set; }
 	}
 }
