097d8aa2b1e5dd19ab57d32880883697cd708f02
Harold <smithhe95@protonmail.com>
2023-09-13 16:54:08 -0400

    Updated request to update a Product and added Endpoint to handle the request

--- Core/Ecommerce.Shared/Requests/Product/UpdateProductApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Product/UpdateProductApiRequest.cs
@@ -1,7 +1,15 @@
+using Ecommerce.Shared.Dtos;
+
 namespace Ecommerce.Shared.Requests.Product
 {
+	/// <summary>
+	/// A Api request to update a Product
+	/// </summary>
 	public class UpdateProductApiRequest
 	{
-		
+		/// <summary>
+		/// The Product to update with
+		/// </summary>
+		public ProductDto? ProductToUpdate { get; set; }
 	}
 }
