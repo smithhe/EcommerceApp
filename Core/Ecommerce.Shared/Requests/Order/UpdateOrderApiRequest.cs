141253546b9c11d1a6865a1cf6e145c6ab782601
Harold <smithhe95@protonmail.com>
2023-09-13 15:46:52 -0400

    Updated request to update a order and added a Endpoint to handle the request

--- Core/Ecommerce.Shared/Requests/Order/UpdateOrderApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Order/UpdateOrderApiRequest.cs
@@ -1,7 +1,15 @@
+using Ecommerce.Shared.Dtos;
+
 namespace Ecommerce.Shared.Requests.Order
 {
+	/// <summary>
+	/// A Api request to update a Order
+	/// </summary>
 	public class UpdateOrderApiRequest
 	{
-		
+		/// <summary>
+		/// The Order to update with
+		/// </summary>
+		public OrderDto? OrderToUpdate { get; set; }
 	}
 }
