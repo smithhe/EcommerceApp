c4fb3ddd346fa0b6f0cdaebc6fbf144bb0ecfdab
Harold <smithhe95@protonmail.com>
2023-09-13 16:58:10 -0400

    Added documentation on the request and endpoint for creating a Review

--- Core/Ecommerce.Shared/Requests/Review/CreateReviewApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Review/CreateReviewApiRequest.cs
@@ -2,9 +2,14 @@ using Ecommerce.Shared.Dtos;
 
 namespace Ecommerce.Shared.Requests.Review
 {
+	/// <summary>
+	/// A Api request to create a new Review
+	/// </summary>
 	public class CreateReviewApiRequest
 	{
-		
+		/// <summary>
+		/// The Review to create
+		/// </summary>
 		public ReviewDto? ReviewToCreate { get; set; }
 	}
 }
