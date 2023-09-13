3bdf71b810980e157b1432ecc67056141a3c6165
Harold <smithhe95@protonmail.com>
2023-09-13 15:34:45 -0400

    Updated request to create a Order and added a Endpoint to handle the request

--- Core/Ecommerce.Shared/Requests/Order/CreateOrderApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Order/CreateOrderApiRequest.cs
@@ -1,7 +1,15 @@
+using Ecommerce.Shared.Dtos;
+
 namespace Ecommerce.Shared.Requests.Order
 {
+	/// <summary>
+	/// A Api request to create a new Order
+	/// </summary>
 	public class CreateOrderApiRequest
 	{
-		
+		/// <summary>
+		/// The Order to be created
+		/// </summary>
+		public OrderDto? OrderToCreate { get; set; }
 	}
 }
