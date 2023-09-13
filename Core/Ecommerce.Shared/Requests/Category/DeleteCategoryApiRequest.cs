dfb6c48a8804250fa9fbc767e60d2ede176763cb
Harold <smithhe95@protonmail.com>
2023-09-13 13:44:13 -0400

    Updated request to delete a Category and added a Endpoint to handle the request

--- Core/Ecommerce.Shared/Requests/Category/DeleteCategoryApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Category/DeleteCategoryApiRequest.cs
@@ -1,7 +1,15 @@
+using Ecommerce.Shared.Dtos;
+
 namespace Ecommerce.Shared.Requests.Category
 {
+	/// <summary>
+	/// A Api request to delete a Category
+	/// </summary>
 	public class DeleteCategoryApiRequest
 	{
-		
+		/// <summary>
+		/// The Category to delete
+		/// </summary>
+		public CategoryDto? CategoryToDelete { get; set; }
 	}
 }
