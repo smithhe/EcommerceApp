c4219aed7daabaff8a87c6a20eb6c7ce99f820d0
Harold <smithhe95@protonmail.com>
2023-09-13 16:38:53 -0400

    Updated request to create a new Product and added Endpoint to handle the request

--- Core/Ecommerce.Shared/Requests/Product/CreateProductApiRequest.cs
+++ Core/Ecommerce.Shared/Requests/Product/CreateProductApiRequest.cs
@@ -1,7 +1,15 @@
+using Ecommerce.Shared.Dtos;
+
 namespace Ecommerce.Shared.Requests.Product
 {
+	/// <summary>
+	/// A Api request to create a new Product
+	/// </summary>
 	public class CreateProductApiRequest
 	{
-		
+		/// <summary>
+		/// The Product to be created
+		/// </summary>
+		public ProductDto? ProductToCreate { get; set; }
 	}
 }
