9a4c2f3cc9abd18c20b66a2c1e9797388bcbece9
Harold <smithhe95@protonmail.com>
2023-09-14 20:52:27 -0400

    Updated Products page to allow a User to add a item to the cart from the page

--- UI/Ecommerce.UI/Pages/Products.razor.cs
+++ UI/Ecommerce.UI/Pages/Products.razor.cs
@@ -1,22 +1,30 @@
+using Blazored.Modal;
+using Blazored.Modal.Services;
 using Blazored.Toast.Services;
 using Ecommerce.Shared.Dtos;
+using Ecommerce.Shared.Responses.CartItem;
 using Ecommerce.Shared.Responses.Product;
 using Ecommerce.UI.Contracts;
+using Ecommerce.UI.Modals;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Ecommerce.UI.Pages
 {
 	public partial class Products
 	{
+		[CascadingParameter] public IModalService Modal { get; set; } = null!;
 		[Parameter] public string CategoryId { get; set; } = null!;
 
 		[Inject] public IProductService ProductService { get; set; } = null!;
 		[Inject] public IToastService ToastService { get; set; } = null!;
+		[Inject] public ICartService CartService { get; set; } = null!;
+		
 		private IEnumerable<ProductDto>? ProductList { get; set; }
-
+		
 		protected override async Task OnInitializedAsync()
 		{
 			GetAllProductsByCategoryIdResponse response = await this.ProductService.GetAllProducts(Convert.ToInt32(this.CategoryId));
@@ -28,6 +36,40 @@ namespace Ecommerce.UI.Pages
 			else
 			{
 				this.ToastService.ShowError(response.Message!);
+			}
+		}
+
+		private async Task AddToCartClick(int productId)
+		{
+			ProductDto product = this.ProductList!.First(p => p.Id == productId);
+
+			ModalParameters parameters = new ModalParameters { { nameof(AddToCartModal.SelectedProduct), product } };
+			
+			IModalReference formModal = this.Modal.Show<AddToCartModal>("Add Item To Cart", parameters);
+			ModalResult result = await formModal.Result;
+
+			if (result.Cancelled)
+			{
+				return;
+			}
+
+			CartItemDto newCartItem = (CartItemDto)result.Data!;
+
+			CreateCartItemResponse response = await this.CartService.AddItemToCart(newCartItem);
+
+			if (response.Success)
+			{
+				this.ToastService.ShowSuccess("Added to cart");
+				return;
+			}
+			
+			this.ToastService.ShowError(response.Message!);
+			if (response.ValidationErrors.Any())
+			{
+				foreach (string validationError in response.ValidationErrors)
+				{
+					this.ToastService.ShowError(validationError);
+				}
 			}
 		}
 	}
