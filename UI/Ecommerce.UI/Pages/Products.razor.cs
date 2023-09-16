9efb0d2e2a933afd17019b63ec423a58a7035e7f
Harold <smithhe95@protonmail.com>
2023-09-15 21:25:23 -0400

    Added redirect to login when user isnt authenticated

--- UI/Ecommerce.UI/Pages/Products.razor.cs
+++ UI/Ecommerce.UI/Pages/Products.razor.cs
@@ -7,6 +7,7 @@ using Ecommerce.Shared.Responses.Product;
 using Ecommerce.UI.Contracts;
 using Ecommerce.UI.Modals;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +17,11 @@ namespace Ecommerce.UI.Pages
 {
 	public partial class Products
 	{
+		[CascadingParameter] private Task<AuthenticationState> AuthenticationState { get; set; } = null!;
 		[CascadingParameter] public IModalService Modal { get; set; } = null!;
 		[Parameter] public string CategoryId { get; set; } = null!;
 
+		[Inject] public NavigationManager NavigationManager { get; set; } = null!;
 		[Inject] public IProductService ProductService { get; set; } = null!;
 		[Inject] public IToastService ToastService { get; set; } = null!;
 		[Inject] public ICartService CartService { get; set; } = null!;
@@ -41,6 +44,14 @@ namespace Ecommerce.UI.Pages
 
 		private async Task AddToCartClick(int productId)
 		{
+			AuthenticationState authState = await this.AuthenticationState;
+
+			if (authState.User.Identity?.IsAuthenticated == false)
+			{
+				this.NavigationManager.NavigateTo("/Login");
+				return;
+			}
+			
 			ProductDto product = this.ProductList!.First(p => p.Id == productId);
 
 			ModalParameters parameters = new ModalParameters { { nameof(AddToCartModal.SelectedProduct), product } };
@@ -63,13 +74,17 @@ namespace Ecommerce.UI.Pages
 				return;
 			}
 			
-			this.ToastService.ShowError(response.Message!);
+			
 			if (response.ValidationErrors.Any())
 			{
 				foreach (string validationError in response.ValidationErrors)
 				{
 					this.ToastService.ShowError(validationError);
 				}
+			}
+			else
+			{
+				this.ToastService.ShowError(response.Message!);
 			}
 		}
 	}
