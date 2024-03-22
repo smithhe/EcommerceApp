aea0a82a17c4b0c4f9123552a4bf6ea8de115558
Harold Smith <smithhe95@protonmail.com>
2024-03-21 21:05:20 -0400

    Added a endpoint for the cancel return from PayPal

---
+++ Api/Ecommerce.FastEndpoints/PayPal/PayPalCancelReturnEndpoint.cs
@@ -0,0 +1,76 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Ecommerce.Application.Features.PayPal.Commands.CancelPayPalOrder;
+using FastEndpoints;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ecommerce.FastEndpoints.PayPal
+{
+    /// <summary>
+    /// A Fast Endpoint implementation that handles a cancel return from PayPal
+    /// </summary>
+    public class PayPalCancelReturnEndpoint : EndpointWithoutRequest
+    {
+        private readonly ILogger<PayPalCancelReturnEndpoint> _logger;
+        private readonly IMediator _mediator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PayPalCancelReturnEndpoint"/> class.
+        /// </summary>
+        /// <param name="logger">The <see cref="ILogger"/> instance used for logging.</param>
+        /// <param name="mediator">The <see cref="IMediator"/> instance used for sending Mediator requests.</param>
+        public PayPalCancelReturnEndpoint(ILogger<PayPalCancelReturnEndpoint> logger, IMediator mediator)
+        {
+            this._logger = logger;
+            this._mediator = mediator;
+        }
+        
+        /// <summary>
+        /// Configures the route and roles for the Endpoint
+        /// </summary>
+        public override void Configure()
+        {
+            this.Get("/api/paypal/checkout/cancel/{returnKey}");
+            this.AllowAnonymous();
+        }
+
+        /// <summary>
+        /// Handles the PayPal cancel return
+        /// </summary>
+        /// <param name="ct">The <see cref="CancellationToken"/> that can be used to request cancellation of the operation.</param>
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            //Log the request to cancel the PayPal return
+            this._logger.LogInformation("Handling request to cancel an order from PayPal");
+            
+            //Get the return key from the route
+            string? returnKey = this.Route<string>("returnKey");
+            
+            //Check if we have a return key
+            if (string.IsNullOrEmpty(returnKey))
+            {
+                //Redirect to the error page in the UI
+                //TODO: Update this to the correct error page when it is created
+                await this.SendRedirectAsync("/error", cancellation: ct);
+                return;
+            }
+            
+            //Cancel the PayPal order
+            bool success = await this._mediator.Send(new CancelPayPalOrderCommand { ReturnKey = returnKey }, ct);
+            
+            //Check if the cancel was successful
+            if (success == false)
+            {
+                //Redirect to the error page in the UI
+                //TODO: Update this to the correct error page when it is created
+                await this.SendRedirectAsync("/error", cancellation: ct);
+            }
+            else
+            {
+                //Redirect to the success page in the UI
+                await this.SendRedirectAsync("/checkout/cancel", cancellation: ct);
+            }
+        }
+    }
+}
