2551fea3add22d0a33fe406e3106570d5ea9091a
Harold Smith <smithhe95@protonmail.com>
2024-03-25 18:13:20 -0400

    Updated the OrderDetail page to use the new OrderItem schema

--- UI/Ecommerce.UI/Pages/OrderDetail.razor.cs
+++ UI/Ecommerce.UI/Pages/OrderDetail.razor.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Blazored.Toast.Services;
 using Ecommerce.Shared.Dtos;
 using Ecommerce.Shared.Responses.Order;
-using Ecommerce.Shared.Responses.Product;
 using Ecommerce.UI.Contracts;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -23,8 +20,6 @@ namespace Ecommerce.UI.Pages
         [Inject] public IProductService ProductService { get; set; } = null!;
         
         private OrderDto? Order { get; set; }
-        private List<ProductDto> OrderProducts { get; set; } = new List<ProductDto>();
-        
         private string? OrderStatus { get; set; }
 
         protected override async Task OnInitializedAsync()
@@ -41,28 +36,6 @@ namespace Ecommerce.UI.Pages
             //Successfully got the order
             this.Order = getOrderByIdResponse.Order;
             this.OrderStatus = Enum.GetName(this.Order!.Status);
-            
-            //Pull the list of product ids in the order
-            IEnumerable<int> productIds = this.Order.OrderItems!.Select(orderItem => orderItem.ProductId).Distinct();
-            
-            //Fetch the information for each product in the order
-            foreach (int productId in productIds)
-            {
-                GetProductByIdResponse getProductByIdResponse = await this.ProductService.GetProductById(productId);
-                
-                //Failed to get the product
-                if (getProductByIdResponse.Success == false)
-                {
-                    this.ToastService.ShowError("Failed to load order products.");
-                    return;
-                }
-                
-                //Successfully got the product
-                ProductDto product = getProductByIdResponse.Product!;
-                
-                //Add the product to the list of products in the order
-                this.OrderProducts.Add(product);
-            }
         }
         
         
