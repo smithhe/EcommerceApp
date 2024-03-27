e4940856c94cc729865599794d946bca61f06849
Harold Smith <smithhe95@protonmail.com>
2024-03-27 18:37:23 -0400

    Updated the 2 worker email messages

--- Core/Ecommerce.Messages/EcommerceUser/SendEmailConfirmationMessage.cs
+++ Core/Ecommerce.Messages/EcommerceUser/SendEmailConfirmationMessage.cs
@@ -1,7 +1,23 @@
 namespace Ecommerce.Messages.EcommerceUser
 {
-    public class SendEmailConfirmationMessage
+    /// <summary>
+    /// Message to send email confirmation after user registration
+    /// </summary>
+    public record SendEmailConfirmationMessage
     {
+        /// <summary>
+        /// Email address to send the email to
+        /// </summary>
+        public string SendTo { get; init; } = null!;
         
+        /// <summary>
+        /// Name of the user
+        /// </summary>
+        public string Name { get; init; } = null!;
+        
+        /// <summary>
+        /// Link to confirm the email
+        /// </summary>
+        public string ConfirmationLink { get; init; } = null!;
     }
 }
--- Core/Ecommerce.Messages/Order/SendOrderConfirmationMessage.cs
+++ Core/Ecommerce.Messages/Order/SendOrderConfirmationMessage.cs
@@ -1,7 +1,36 @@
+using System.Collections.Generic;
+using Ecommerce.Shared.Dtos;
+
 namespace Ecommerce.Messages.Order
 {
+    /// <summary>
+    /// Message to send an order confirmation
+    /// </summary>
     public class SendOrderConfirmationMessage
     {
+        /// <summary>
+        /// The email address to send the order confirmation to
+        /// </summary>
+        public string SendTo { get; init; } = null!;
         
+        /// <summary>
+        /// Name of the customer
+        /// </summary>
+        public string Name { get; set; } = null!;
+        
+        /// <summary>
+        /// Total amount of the order
+        /// </summary>
+        public double Total { get; set; }
+
+        /// <summary>
+        /// Order number of the order
+        /// </summary>
+        public string OrderNumber { get; set; } = null!;
+
+        /// <summary>
+        /// Order items of the order
+        /// </summary>
+        public IEnumerable<OrderItemDto> OrderItems { get; set; } = null!;
     }
 }
