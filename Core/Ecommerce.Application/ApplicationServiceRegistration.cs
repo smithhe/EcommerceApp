50fcf8d0afc2183e2bccb4ba35de54f939591c18
Harold Smith <smithhe95@protonmail.com>
2024-03-28 17:46:16 -0400

    Added MassTransit to the Application class library service registration

--- Core/Ecommerce.Application/ApplicationServiceRegistration.cs
+++ Core/Ecommerce.Application/ApplicationServiceRegistration.cs
@@ -1,7 +1,9 @@
+using System;
 using Ecommerce.Persistence;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using Ecommerce.PayPal;
+using MassTransit;
 using Microsoft.Extensions.Configuration;
 
 namespace Ecommerce.Application
@@ -11,6 +13,33 @@ namespace Ecommerce.Application
 		public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
 		{
 			services.AddAutoMapper(Assembly.GetExecutingAssembly());
+			
+			services.AddMassTransit(bus =>
+			{
+				bus.SetKebabCaseEndpointNameFormatter();
+				
+				string? uri = configuration["RabbitMQ:Uri"];
+				string? username = configuration["RabbitMQ:Username"];
+				string? password = configuration["RabbitMQ:Password"];
+
+				if (string.IsNullOrEmpty(uri) || string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+				{
+					Console.WriteLine("Config values not set for Rabbit URI, Username, or Password");
+					Console.WriteLine("Exiting....");
+					Environment.Exit(1);
+				}
+				
+				bus.UsingRabbitMq((context, cfg) =>
+				{
+					cfg.Host(uri, "/", h => {
+						h.Username(username);
+						h.Password(password);
+					});
+
+					cfg.ConfigureEndpoints(context);
+				});
+			});
+			
 			services.AddMediatR(cfg =>
 			{
 				cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
