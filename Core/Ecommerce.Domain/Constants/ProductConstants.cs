ab82b2db31c2031c5ab51f9961a70215e34ab770
Harold Smith <smithhe95@protonmail.com>
2024-04-13 20:24:45 -0400

    Added init constants file for Product to be used in CartItem testing

---
+++ Core/Ecommerce.Domain/Constants/ProductConstants.cs
@@ -0,0 +1,10 @@
+namespace Ecommerce.Domain.Constants
+{
+    public class ProductConstants
+    {
+        
+        public const string _getProductByIdSuccessMessage = "Successfully retrieved the product.";
+        public const string _getProductByIdErrorMessage = "Error retrieving the product, please try again later.";
+        
+    }
+}
