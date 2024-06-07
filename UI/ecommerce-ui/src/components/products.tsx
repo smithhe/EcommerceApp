5e9117bf1fb9ad198bdd61921ee80f266fc09592
Harold Smith <smithhe95@protonmail.com>
2024-06-06 23:41:01 -0400

    Added the AddToCart Modal to products

--- UI/ecommerce-ui/src/components/products.tsx
+++ UI/ecommerce-ui/src/components/products.tsx
@@ -7,12 +7,21 @@ import StarRatings from "./childComponents/starratings.tsx";
 import '../styles/Products.css'
 import {useAuth} from "../AuthContext.tsx";
 import productService from "../services/ProductService";
+import Modal from "react-modal";
+import cartService from "../services/CartService.ts";
+import {CartItem} from "../models/CartItem.ts";
 
 const Products = () => {
     const [productList, setProductList] = useState<Product[]>([]);
     const { categoryId } = useParams<{ categoryId: string }>();
-    const { isAuthenticated } = useAuth();
+    const { isAuthenticated, claims } = useAuth();
     const navigate = useNavigate();
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [modalCount, setModalCount] = useState<number>(0);
+    const [modalProductId, setModalProductId] = useState<number>(0);
+    const [modalProductName, setModalProductName] = useState<string>('');
+    const [modalProductDesc, setModalProductDesc] = useState<string>('');
+
 
     useEffect(() => {
         if (!categoryId) {
@@ -37,9 +46,47 @@ const Products = () => {
             navigate('/login');
         }
 
-        toast.info('Adding to cart');
-        console.log(productId);
+        const product = productList.find((p) => p.id === productId);
+
+        if (product == undefined)
+        {
+            toast.error('Invalid product ID');
+            return;
+        }
+
+        setModalProductName(product.name);
+        setModalProductDesc(product.description);
+        setModalProductId(productId);
+        setIsOpen(true);
     };
+
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+        },
+    };
+
+    const closeModal = async () => {
+        setIsOpen(false);
+        const response = await cartService.addItemToCart( new CartItem(0, modalProductId || 0, claims?.["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"] || '', modalCount));
+
+        if (response.success)
+        {
+            toast.success(response.message);
+            return;
+        }
+
+        toast.error(response.message);
+    }
+
+    const cancelCloseModal = () => {
+        setIsOpen(false);
+    }
 
     if (productList.length === 0) {
         return <p><em>Loading...</em></p>;
@@ -77,6 +124,25 @@ const Products = () => {
                     </div>
                 ))}
             </div>
+            <Modal isOpen={modalIsOpen} style={customStyles} contentLabel="Add To Cart">
+                <div className="simple-form mx-4 ">
+                    <div className="row text-center">
+                        <h1 className="card-title">{modalProductName}</h1>
+                        <p className="card-text mt-2">{modalProductDesc}</p>
+                    </div>
+                    <div className="row mt-3">
+                        <div className="form-group text-center">
+                            <label htmlFor="quantity" className="h5">Quantity:</label>
+                            <input type="number" className="form-control" id="quantity" name="quantity" min="1" onChange={(e) => setModalCount(Number(e.target.value))}/>
+                        </div>
+                    </div>
+
+                    <div className="row mt-4">
+                        <button type="submit" className="btn btn-primary form-control" onClick={closeModal}>Add to Cart</button>
+                        <button className="btn btn-secondary form-control mt-2" onClick={cancelCloseModal}>Cancel</button>
+                    </div>
+                </div>
+            </Modal>
             <ToastContainer />
         </div>
     );
