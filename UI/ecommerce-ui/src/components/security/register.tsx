a49b3808f73a15594afcf0d769be965d060b12ca
Harold Smith <smithhe95@protonmail.com>
2024-06-09 16:04:26 -0400

    Fixed page refresh bug on register page

--- UI/ecommerce-ui/src/components/security/register.tsx
+++ UI/ecommerce-ui/src/components/security/register.tsx
@@ -63,65 +63,63 @@ const Register: React.FC = () => {
                 <div className="col-md-6">
                     <div className="login-form">
                         <h2 className="text-center mb-4">Login</h2>
-                        <form onSubmit={handleSubmit}>
-                            <div className="form-group">
-                                <label htmlFor="username" className="form-label h5">Username</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="username" className="form-label h5">Username</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
 
-                            <div className="form-group">
-                                <label htmlFor="username" className="form-label h5">First Name</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    value={firstName}
-                                    onChange={(e) => setFirstName(e.target.value)}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="username" className="form-label h5">First Name</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </div>
 
-                            <div className="form-group">
-                                <label htmlFor="username" className="form-label h5">Last Name</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    value={lastName}
-                                    onChange={(e) => setLastName(e.target.value)}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="username" className="form-label h5">Last Name</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </div>
 
-                            <div className="form-group">
-                                <label htmlFor="username" className="form-label h5">Email</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="username" className="form-label h5">Email</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
 
-                            <div className="form-group">
-                                <label htmlFor="password" className="form-label h5">Password</label>
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </div>
-                            {isProcessing ? (
-                                <button type="submit" className="btn btn-primary form-control mt-3">
-                                    <span className="spinner-border spinner-border-sm" role="status"></span>
-                                    Registering
-                                </button>
-                            ) : (
-                                <button type="submit" className="btn btn-primary form-control mt-3">Register</button>
-                            )}
-                        </form>
+                        <div className="form-group">
+                            <label htmlFor="password" className="form-label h5">Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        {isProcessing ? (
+                            <button type="button" className="btn btn-primary form-control mt-3">
+                                <span className="spinner-border spinner-border-sm" role="status"></span>
+                                Registering
+                            </button>
+                        ) : (
+                            <button type="button" className="btn btn-primary form-control mt-3" onClick={handleSubmit}>Register</button>
+                        )}
                     </div>
                 </div>
 
